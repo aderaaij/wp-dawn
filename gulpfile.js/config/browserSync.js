6d77601d28d08d647bc89276d327d04b0bb85845
Arden de Raaij <a.de.raaij@gmail.com>
2017-09-22 22:09:51 +0100

    [add] browsersync proxy

--- gulpfile.js/config/browserSync.js
+++ gulpfile.js/config/browserSync.js
@@ -1,6 +1,18 @@
 const config = require('./');
 
 module.exports = {
-    proxy: `http://${config.appUrl}`,
-    open: false,
+    proxy: config.appUrl,
+    host: config.appUrl,
+    port: 4040,
+    open: 'external',
+    files: [
+        `${config.themeAssets}**/*.js`,
+        `${config.themeAssets}**/*.css`,
+        `${config.themePath}**/*.php`,
+        `${config.themeAssets}**/*.jpg`,
+        `${config.themeAssets}**/*.jpeg`,
+        `${config.themeAssets}**/*.png`,
+        `${config.themeAssets}**/*.svg`,
+        `${config.themeAssets}**/*.gif`,
+    ],
 };
