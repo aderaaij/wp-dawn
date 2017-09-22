97d8a04fd2ee86462851877531f2fedd900cdc4c
Arden de Raaij <a.de.raaij@gmail.com>
2017-09-22 16:46:06 +0100

    [update] babel presets env

--- gulpfile.js/lib/webpack-multi-config.js
+++ gulpfile.js/lib/webpack-multi-config.js
@@ -20,7 +20,13 @@ const webpackExports = (env) => {
                     query: {
                         // https://github.com/babel/babel-loader#options
                         cacheDirectory: true,
-                        presets: ['es2015', 'stage-2'],
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: ['last 2 versions'],
+                                },
+                            }],
+                        ],
                     },
                     exclude: /(node_modules|bower_components)/,
                 },
