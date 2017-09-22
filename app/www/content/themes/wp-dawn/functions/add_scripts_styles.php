06e11268ea2318b4c69554acbc0fdb50d5618482
Arden de Raaij <a.de.raaij@gmail.com>
2017-09-22 22:23:37 +0100

    [split] functions

---
+++ app/www/content/themes/wp-dawn/functions/add_scripts_styles.php
@@ -0,0 +1,26 @@
+<?php
+// =========================================================================
+// Cache busting function
+// =========================================================================
+function autoversion($url) {
+    $path = pathinfo($url);
+    $ver = filemtime(get_theme_file_path().$url);
+    return $ver;
+}
+
+// =========================================================================
+// REGISTER / DEREGISTER SCRIPTS AND STYLES
+// =========================================================================
+function add_scripts_styles() {
+    global $wp_styles;
+    // Register script
+    wp_register_script( 'script', get_template_directory_uri() . '/assets/js/script.js','', autoversion('/assets/js/script.js'), true );
+    wp_enqueue_script('script');
+
+    wp_enqueue_style( 'style',  get_template_directory_uri() . '/assets/css/screen.css','', autoversion('/assets/css/screen.css'), '');
+
+    // Remove wp-embed script
+    wp_deregister_script( 'wp-embed' );
+    
+}
+add_action( 'wp_enqueue_scripts', 'add_scripts_styles' );
---
+++ app/www/content/themes/wp-dawn/functions/remove_wp_emojicons_tinymce.php
@@ -0,0 +1,11 @@
+<?php
+// =========================================================================
+// DISABLE EMOJI FROM TINYMCE
+// =========================================================================
+function disable_emojicons_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
